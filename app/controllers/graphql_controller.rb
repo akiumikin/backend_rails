3e29294c99e275f02112387235282c2f48bd8eb0
akiumikin <akiumikin@gmail.com>
2023-05-05 14:16:53 +0000

    cors関連を調整

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -4,6 +4,8 @@ class GraphqlController < ApplicationController
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
 
+  protect_from_forgery except: [:execute]
+
   def execute
     variables = prepare_variables(params[:variables])
     query = params[:query]
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    if Rails.env.production?
-      origins 'https://server.akiumi.net', 'https://www.akiumi.net', 'http://localhost:3000'
-    end
+    origins 'https://server.akiumi.net', 'https://www.akiumi.net', 'http://localhost:3000'
 
     resource '*',
              headers: :any,
